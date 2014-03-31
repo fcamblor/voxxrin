2d2fec74c4bf2ea4106a818582393eeb1e3fc6dd
fcamblor <frederic.camblor@4sh.fr>
2014-03-31 21:10:05 +0200

    updated voxxrdroid webview to enable remote debug

--- voxxr-droid/src/vooxr/droid/VoxxrDroid.java
+++ voxxr-droid/src/vooxr/droid/VoxxrDroid.java
@@ -3,11 +3,23 @@ package vooxr.droid;
 import android.os.Bundle;
 import com.phonegap.DroidGap;
 import voxxr.droid.R;
+import android.os.Build;
+import android.util.Log;
+import android.content.pm.ApplicationInfo;
+import android.webkit.WebView;
 
 public class VoxxrDroid extends DroidGap {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+          if(0 != (getApplicationInfo().flags = ApplicationInfo.FLAG_DEBUGGABLE)){
+            Log.i("Your app", "Enabling web debugging");
+            WebView.setWebContentsDebuggingEnabled(true);
+          }
+        }
+
         super.setIntegerProperty("splashscreen", R.drawable.splash);
         super.loadUrl("file:///android_asset/www/m.html");
     }
